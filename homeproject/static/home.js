1e554f268b8d8b6a19a554bfd93584bfaf14e03a
asadsamadsheikh <asadsamadsheikh@gmail.com>
2026-02-13 23:59:07 +0530

    back to top changes

--- homeproject/static/home.js
+++ homeproject/static/home.js
@@ -28,3 +28,13 @@ function sendMessage() {
 
     input.value = "";
 }
+
+
+window.addEventListener("scroll", function () {
+    const btn = document.querySelector(".topbtn");
+    if (window.scrollY > 200) {
+        btn.classList.add("show");
+    } else {
+        btn.classList.remove("show");
+    }
+});
